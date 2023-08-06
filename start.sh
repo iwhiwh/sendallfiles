b183d318ae8cb7f68086345abf19e0a52d69a103
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-08-06 13:59:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Apnamovi/LastBot.git /LastBot
+  git clone https://github.com/iwhiwh/sendallfiles /sendallfiles
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LastBot
+  git clone $UPSTREAM_REPO /sendallfiles
 fi
 cd /LastBot
 pip3 install -U -r requirements.txt
